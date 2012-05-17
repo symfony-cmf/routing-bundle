515271a80770bd99fa8983635b9c92bfd471674f
lsmith77 <smith@pooteeweet.org>
2012-05-18 00:25:41 +0200

    fixed autoload for tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,22 +1,11 @@
 <?php
 
-$file = __DIR__.'/../vendor/.composer/autoload.php';
+$file = __DIR__.'/../vendor/autoload.php';
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
 
 require_once $file;
-
-spl_autoload_register(function($class) {
-    if (0 === strpos($class, 'Symfony\\Cmf\\Bundle\\RoutingExtraBundle\\')) {
-        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 4)).'.php';
-        if (!stream_resolve_include_path($path)) {
-            return false;
-        }
-        require_once $path;
-        return true;
-    }
-});
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
