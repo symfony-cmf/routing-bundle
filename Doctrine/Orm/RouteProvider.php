65af83edd0c3e8060298548d4c4320453bf12422
David Buchmann <david.buchmann@liip.ch>
2013-06-20 11:15:58 +0200

    WIP orm provider and content repository

---
+++ Doctrine/Orm/RouteProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Symfony\Cmf\Bundle\RoutingBundle\Doctrine\Orm;
+
+use Symfony\Component\Routing\Route as SymfonyRoute;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Cmf\Component\Routing\RouteProviderInterface;
+
+/**
+ * Provider loading routes from Doctrine
+ *
+ * This is <strong>NOT</strong> not a doctrine repository but just the route
+ * provider for the NestedMatcher. (you could of course implement this
+ * interface in a repository class, if you need that)
+ *
+ * @author david.buchmann@liip.ch
+ */
+class RouteProvider extends DoctrineProvider implements RouteProviderInterface
+{
+    protected function getCandidates($url)
+    {
+        $candidates = array();
+        if ('/' !== $url) {
+            if (preg_match('/(.+)\.[a-z]+$/i', $url, $matches)) {
+                $candidates[] = $url;
+                $url = $matches[1];
+            }
+
+            $part = $url;
+            while (false !== ($pos = strrpos($part, '/'))) {
+                $candidates[] = $part;
+                $part = substr($url, 0, $pos);
+            }
+        }
+
+        $candidates[] = '/';
+
+        return $candidates;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getRouteByName($name, $parameters = array())
+    {
+    }
+
+    public function getRoutesByNames($names, $parameters = array())
+    {
+    }
+}
