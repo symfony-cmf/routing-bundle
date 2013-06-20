20f0c68ae3fcf6de373395e55f62be993b2209a8
David Buchmann <david.buchmann@liip.ch>
2013-06-20 11:15:58 +0200

    WIP orm provider and content repository

---
+++ Doctrine/Orm/ContentRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Symfony\Cmf\Bundle\RoutingBundle\Doctrine\Orm;
+
+/**
+ * Abstract content repository for PHPCR-ODM
+ *
+ * @author teito
+ */
+class ContentRepository extends DoctrineProvider implements ContentRepositoryInterface
+{
+    /**
+     * Determine target class and id for this content
+     *
+     * @param mixed $identifier as produced by getContentId
+     *
+     * @return array with model first element, id second
+     */
+    protected function getModelAndId($identifier)
+    {
+        return explode(':', $identifier, 2);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function findById($id)
+    {
+        list($model, $modelId) = $this->getModelAndId($id);
+
+        return $this->getObjectManager()->getRepository($model)->find($modelId);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getContentId($content)
+    {
+        if (! is_object($content)) {
+            return null;
+        }
+
+        try {
+            $meta = $this->getObjectManager()->getClassMetadata(get_class($content));
+            $ids = $meta->getIdentifierValues($content);
+            if (0 !== count($ids)) {
+                throw new \Exception('Multi identifier values not supported in ' . get_class($content));
+            }
+            return implode(':', array(
+                get_class($content),
+                reset($ids)
+            ));
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+}
