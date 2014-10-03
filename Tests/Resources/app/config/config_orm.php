c2cc711fc272931805dd212493fb7e42f75034e8
Lukas Kahwe Smith <smith@pooteeweet.org>
2014-10-03 11:33:37 +0200

    fix tests

--- Tests/Resources/app/config/config_orm.php
+++ Tests/Resources/app/config/config_orm.php
@@ -1,5 +1,6 @@
 <?php
 
+$container->setParameter('cmf_testing.bundle_fqn', 'Symfony\Cmf\Bundle\RoutingBundle');
 $loader->import(CMF_TEST_CONFIG_DIR.'/default.php');
 $loader->import(CMF_TEST_CONFIG_DIR.'/doctrine_orm.php');
 $loader->import(__DIR__.'/cmf_routing.yml');
--- Tests/Resources/app/config/config_phpcr.php
+++ Tests/Resources/app/config/config_phpcr.php
@@ -1,5 +1,6 @@
 <?php
 
+$container->setParameter('cmf_testing.bundle_fqn', 'Symfony\Cmf\Bundle\RoutingBundle');
 $loader->import(CMF_TEST_CONFIG_DIR.'/default.php');
 $loader->import(CMF_TEST_CONFIG_DIR.'/phpcr_odm.php');
 $loader->import(CMF_TEST_CONFIG_DIR.'/sonata_admin.php');
