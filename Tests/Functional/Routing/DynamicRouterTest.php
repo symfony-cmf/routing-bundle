5dc7b8bdec81acbc9baa6cefa9a294b99984058f
WouterJ <waldio.webdesign@gmail.com>
2015-12-02 11:40:40 +0100

    Fix Symfony 3 compatibility

--- Tests/Functional/Routing/DynamicRouterTest.php
+++ Tests/Functional/Routing/DynamicRouterTest.php
@@ -13,6 +13,7 @@ namespace Symfony\Cmf\Bundle\RoutingBundle\Tests\Functional\Routing;
 
 use PHPCR\Util\NodeHelper;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Cmf\Component\Routing\ChainRouter;
 use Symfony\Cmf\Component\Routing\RouteObjectInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
