d57e9d76fe185176a0129c9236e4dc57abadf89f
ElectricMaxxx <maximilian.berghoff@gmx.de>
2016-05-02 22:12:08 +0200

    change controller_by_class configuration to get a action by http method configuration

--- Tests/Resources/Fixtures/config/config.php
+++ Tests/Resources/Fixtures/config/config.php
@@ -15,6 +15,13 @@ $container->loadFromExtension('cmf_routing', array(
         ),
         'controllers_by_class' => array(
             'Symfony\Cmf\Bundle\ContentBundle\Document\StaticContent' => 'cmf_content.controller:indexAction',
+            'My\Class' => array(
+                array('methods' => array('put', 'post'), 'controller' => 'service:method'),
+                array('methods' => array('any'), 'controller' => 'service:readMethod'),
+            ),
+            'Other\Class' => array(
+                array('methods' => array('any'), 'controller' => 'service:method'),
+            ),
         ),
         'templates_by_class' => array(
             'Symfony\Cmf\Bundle\ContentBundle\Document\StaticContent' => 'CmfContentBundle:StaticContent:index.html.twig',
