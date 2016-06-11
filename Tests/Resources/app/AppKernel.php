4d5e36e51b94c35ccfcd4a92ec09a59dc7f8d127
ElectricMaxxx <maximilian.berghoff@gmx.de>
2016-06-11 12:01:16 +0200

    remove almost everything

--- Tests/Resources/app/AppKernel.php
+++ Tests/Resources/app/AppKernel.php
@@ -12,7 +12,6 @@ class AppKernel extends TestKernel
         if ('phpcr' === $this->environment) {
             $this->requireBundleSets(array(
                 'phpcr_odm',
-                'sonata_admin_phpcr',
             ));
         } elseif ('orm' === $this->environment) {
             $this->requireBundleSet('doctrine_orm');
