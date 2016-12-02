a28fe0f6287d005095671cc518e28834cba826b5
ElectricMaxxx <maximilian.berghoff@gmx.de>
2016-12-02 13:21:37 +0100

    only register validation configuration for phpcr if phpcr is activated

---
+++ DependencyInjection/Compiler/ValidationPass.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2015 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Cmf\Bundle\RoutingBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+/**
+ * Register validation files if their storage is activated.
+ */
+class ValidationPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->hasParameter('cmf_routing.backend_type_phpcr')) {
+            $container
+                ->getDefinition('validator.builder')
+                ->addMethodCall('addXmlMappings', [[__DIR__.'../../Resources/config/validation-phpcr.xml']]);
+        }
+    }
+}
