64b284259837c4cc52c89536cd3949147cd08ecc
Maximilian Berghoff <Maximilian.berghoff@gmx.de>
2017-11-23 20:44:07 +0100

    fix reference usage in compile pass (#408)

--- src/DependencyInjection/Compiler/TemplatingValidatorPass.php
+++ src/DependencyInjection/Compiler/TemplatingValidatorPass.php
@@ -14,6 +14,7 @@ namespace Symfony\Cmf\Bundle\RoutingBundle\DependencyInjection\Compiler;
 use Symfony\Cmf\Bundle\RoutingBundle\Validator\Constraints\RouteDefaultsTemplatingValidator;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * To avoid a BC-Break: If templating component exists, we will use the validator using general templating
@@ -29,10 +30,9 @@ class TemplatingValidatorPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         if ($container->has('templating')) {
-            $templatingDefinition = $container->findDefinition('templating');
             $validatorDefinition = $container->getDefinition('cmf_routing.validator.route_defaults');
             $validatorDefinition->setClass(RouteDefaultsTemplatingValidator::class);
-            $validatorDefinition->replaceArgument(1, $templatingDefinition);
+            $validatorDefinition->replaceArgument(1, new Reference('templating'));
         }
     }
 }
