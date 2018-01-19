symfony-cmf/routing-bundle

commit ceef4bfaaf3b15a6004bea0af6ced378980665f6

Fixed #413: Made PHPCR documents validation optional (#414)
This validation is done only when the Symfony validator builder is available.

src/DependencyInjection/Compiler/ValidationPass.php
@@ -21,7 +21,7 @@ class ValidationPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        if ($container->hasParameter('cmf_routing.backend_type_phpcr')) {
+        if ($container->hasParameter('cmf_routing.backend_type_phpcr' && $container->hasDefinition('validator.builder'))) {
             $container
                 ->getDefinition('validator.builder')
                 ->addMethodCall('addXmlMappings', [[__DIR__.'/../../Resources/config/validation-phpcr.xml']]);

commit f7dd7ecdee0d3c89a238b7414da449ec073e2448

Fixed typo introduced in #414

src/DependencyInjection/Compiler/ValidationPass.php
@@ -21,7 +21,7 @@ class ValidationPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        if ($container->hasParameter('cmf_routing.backend_type_phpcr' && $container->hasDefinition('validator.builder'))) {
+        if ($container->hasParameter('cmf_routing.backend_type_phpcr') && $container->hasDefinition('validator.builder')) {
             $container
                 ->getDefinition('validator.builder')
                 ->addMethodCall('addXmlMappings', [[__DIR__.'/../../Resources/config/validation-phpcr.xml']]);