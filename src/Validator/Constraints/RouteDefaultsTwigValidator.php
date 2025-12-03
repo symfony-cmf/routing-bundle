da828b48a30b70a89e2ac35d96ea41d95dc9e3c1
David Buchmann <david@liip.ch>
2025-12-03 10:33:48 +0100

    revert renaming parameter

--- src/Validator/Constraints/RouteDefaultsTwigValidator.php
+++ src/Validator/Constraints/RouteDefaultsTwigValidator.php
@@ -31,7 +31,7 @@ class RouteDefaultsTwigValidator extends ConstraintValidator
         $this->twig = $twig;
     }
 
-    public function validate(mixed $value, Constraint $constraint): void
+    public function validate(mixed $defaults, Constraint $constraint): void
     {
         if (!$constraint instanceof RouteDefaults) {
             throw new \InvalidArgumentException(sprintf('Expected %s, got %s', RouteDefaults::class, get_class($constraint)));
